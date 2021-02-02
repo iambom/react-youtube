fda92dd90a464ac948c1b724a2d54dad7bb7b791
보미니 <iambxmk@gmail.com>
2021-02-02 11:51:21 +0900

    Add .env and refactorying search func

--- src/app.jsx
+++ src/app.jsx
@@ -3,33 +3,19 @@ import styles from './app.module.css';
 import Search from "./components/Search";
 import ListContainer from './container/ListContainer';
 
-const App = () => {
+const App = ({youtube}) => {
   const [videos, setVideos] = useState([]);
+
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyD_A-MIp774wIMuIm-fPIAPnZaJvG6aVx0`, requestOptions)
-      .then(response => response.json())
-      // 검색 api는 기존 api와 달라서 걸러서 기존 있는 아이템 정보 유지하고 id를 object 안에 있는 videoID로 덮어줌
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
   }
 
-
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD_A-MIp774wIMuIm-fPIAPnZaJvG6aVx0", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log("erroe", error));
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
   }, []);
 
   return (
--- src/container/ListContainer.jsx
+++ src/container/ListContainer.jsx
@@ -1,19 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import VideoList from '../components/VideoList';
 import styles from './ListContainer.module.css';
 
 const ListContainer = ({videos, }) => {
-
     return (
-        <ul className={styles.videos}>
-            {
-                videos.map(video => {
-                    return(
-                        <VideoList video={video} key={video.id}/>
-                    )
-                })
-            }
-        </ul>
+    <ul className={styles.videos}>
+        {
+            videos.map(video => {
+                return (<VideoList video={video} key={video.id}/>)
+            })
+        }
+    </ul>
     )
 }
 
